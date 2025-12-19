6bab946bb4cbc3ea69e61501962b27ec5823a91e
Phụng Trần <44075162+phungtd@users.noreply.github.com>
2025-12-19 19:51:32 +0000

    v1.0.1

--- crystalrosegame.user.js
+++ crystalrosegame.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         crystalrosegame
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://crystalrosegame.wildrift.leagueoflegends.com
